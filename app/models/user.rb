6e3b012e677da196ac1082cc1c72d28d5d924186
Thomas Morris <thomas.imorris@gmail.com>
2013-09-26 13:30:12 +0900

    Create sign in for valid user   -Remember cookies   -Make layout   -Make tests   -Include bootstrap javascript

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
-    SecureRandom.urlsave_base64
+    SecureRandom.urlsafe_base64
   end
 
   def User.encrypt(token)
