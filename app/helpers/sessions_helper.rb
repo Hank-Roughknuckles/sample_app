4a804cdd354edb1ed57cbae06f1ceee34f944362
Thomas Morris <thomas.imorris@gmail.com>
2014-04-26 20:08:24 +0300

    Complete 8.2.4. Time to move on to 8.2.5

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-  def sign_in
+  def sign_in (user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
@@ -12,6 +12,10 @@ module SessionsHelper
 
   def current_user
     remember_token = User.digest(cookies[:remember_token])
-    @current_user = ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def signed_in?
+    !current_user.nil?
   end
 end
