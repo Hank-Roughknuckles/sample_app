5d8ac660c35d15a7c9ebe37b5e498cbbf8f6632b
Thomas Morris <thomas.imorris@gmail.com>
2014-04-26 22:23:52 +0300

    Finish chapter 8 completely. Time for Chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,7 +13,7 @@ describe "AuthenticationPages" do
       before { visit signin_path }
 
       describe "with invalid information" do
-        before { click_button "Sign in" }
+        before { invalid_signin() }
 
         it { should have_title "Sign in" }
         it { should have_selector "div.alert.alert-error" }
@@ -27,9 +27,7 @@ describe "AuthenticationPages" do
       describe "with valid information" do
         let(:user) { FactoryGirl.create(:user) }
         before do
-          fill_in "Email",      with: user.email.upcase
-          fill_in "Password",   with: user.password
-          click_button "Sign in"
+          valid_signin( user )
         end
 
         it { should have_title user.name }
