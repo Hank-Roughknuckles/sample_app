ef3cfefd86b50e41d2a086555cdc65312a00cc38
Thomas Morris <thomas.imorris@gmail.com>
2014-04-30 12:08:32 +0300

    time for 11.2.4

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -67,6 +67,15 @@ describe "AuthenticationPages" do
             before { visit users_path }
             it { should have_title "Sign in" }
           end
+
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_title 'Sign in' }
+          end
+          describe "visitng the followers page" do
+            before { visit followers_user_path(user) }
+            it { should have_title 'Sign in' }
+          end
         end
 
         describe "when attempting to visit a protected page" do
